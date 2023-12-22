5a7d02df5fe2144931824919091464c7d71b6d4d
Aditya Prakash <44158175+chauhan07@users.noreply.github.com>
2023-12-22 13:52:37 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -124,99 +124,102 @@ if(pageName[0] == "countries"){
 }
 
 if(pageName[0] == "explore-country"){
-    queryString = pathName.search.slice(1);
-    // Split the query string into an array of key-value pairs
-    var queryParams = queryString.split('&');
-
-    var params = {};
-    for (var i = 0; i < queryParams.length; i++) {
-        var pair = queryParams[i].split('=');
-        var key = decodeURIComponent(pair[0]);
-        var value = decodeURIComponent(pair[1] || '');
-        params[key] = value;
-    }
-
-    // get search country usin fetch api
-    function makeAjaxRequest(url, method, callback) {
-        var xhr = new XMLHttpRequest();
-
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                callback(JSON.parse(xhr.responseText));
-            }
-        };
-
-        xhr.open(method, url, true);
-        xhr.send();
-    }
+    if(pathName.search != ""){
+        queryString = pathName.search.slice(1);
+        // Split the query string into an array of key-value pairs
+        var queryParams = queryString.split('&');
     
-    // Example: Making an AJAX GET request
-    // console.log(params['country-name'].split("+").join(" "));
-    params['country-name'] = params['country-name'].split("+").join(" ")
-    var apiUrl = `https://restcountries.com/v3.1/name/${params['country-name']}`;
-
-    makeAjaxRequest(apiUrl, 'GET', function(data) {
-        printHTML(data[0])
-    });
-
-    function printHTML(countrydata){
-        // console.log(countrydata);
-        document.getElementById("countryTitle").innerText = `${countrydata.name.official} | Jags Fact`
-        document.getElementById("countryFlag").setAttribute("src",countrydata.flags.svg)
-        document.getElementById("officialName").innerHTML = countrydata.name.official  ; //put coat of arm here
-        document.querySelector(".oName img").setAttribute("src",countrydata.coatOfArms.png)
-
-        // native name
-        var nativeName = countrydata.name.nativeName;
-        var nativeNameList = "";
-        for (let x in nativeName) {
-            if(x != "eng"){
-                // console.log(nativeName[x].official);
-                nativeNameList += `<i>${nativeName[x].official}</i>`;
-            }
+        var params = {};
+        for (var i = 0; i < queryParams.length; i++) {
+            var pair = queryParams[i].split('=');
+            var key = decodeURIComponent(pair[0]);
+            var value = decodeURIComponent(pair[1] || '');
+            params[key] = value;
         }
-        document.getElementById("nativeNames").innerHTML = nativeNameList;
-        document.getElementById("commonName").innerText = countrydata.name.common;
-        document.getElementById("capitalName").innerText = countrydata.capital[0];
-        document.getElementById("contientName").innerText = countrydata.continents[0];
-
-        // maps
-        var mapsHref = document.querySelectorAll(".mapsHdng a");
-        mapsHref[0].setAttribute("href", countrydata.maps.googleMaps);
-        mapsHref[1].setAttribute("href", countrydata.maps.openStreetMaps);
-
-        // other information
-        let popinCommas = addCommas(countrydata.population);
-        document.getElementById("population").innerText = popinCommas;
-        let areainCommas = addCommas(countrydata.area);
-        document.getElementById("area").innerHTML = areainCommas + " km<sup>2</sup>";
-        document.getElementById("idd").innerText = countrydata.idd.root + countrydata.idd.suffixes[0];
-        document.getElementById("tld").innerText = countrydata.tld;
-
-        // is un member 
-        if(countrydata.unMember == true){
-            document.getElementById("unMember").innerHTML = "<i class='info'>yes</i>";
-        } else{
-            document.getElementById("unMember").innerHTML = "<i class='warning'>no</i>";
-        }
-
-        // currency 
-        let currHTML = "";
-        let currList = countrydata.currencies;
-        for (let x in currList) {
-            currHTML += `<i class="currname">${currList[x].name}</i>`
-        }
-
-        document.getElementById("curr").innerHTML = currHTML;
-        document.getElementById("carSide").innerText = countrydata.car.side;
-        document.getElementById("startOfWeek").innerText = countrydata.startOfWeek;
-
-    }
-    // add comma to a bigger numbers
-    function addCommas(number) {
-        let numberString = number.toString();
-        numberString = numberString.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-        return numberString;
-    }
     
+        // get search country usin fetch api
+        function makeAjaxRequest(url, method, callback) {
+            var xhr = new XMLHttpRequest();
+    
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    callback(JSON.parse(xhr.responseText));
+                }
+            };
+    
+            xhr.open(method, url, true);
+            xhr.send();
+        }
+        
+        // Example: Making an AJAX GET request
+        // console.log(params['country-name'].split("+").join(" "));
+        params['country-name'] = params['country-name'].split("+").join(" ")
+        var apiUrl = `https://restcountries.com/v3.1/name/${params['country-name']}`;
+    
+        makeAjaxRequest(apiUrl, 'GET', function(data) {
+            printHTML(data[0])
+        });
+    
+        function printHTML(countrydata){
+            // console.log(countrydata);
+            document.getElementById("countryTitle").innerText = `${countrydata.name.official} | Jags Fact`
+            document.getElementById("countryFlag").setAttribute("src",countrydata.flags.svg)
+            document.getElementById("officialName").innerHTML = countrydata.name.official  ; //put coat of arm here
+            document.querySelector(".oName img").setAttribute("src",countrydata.coatOfArms.png)
+    
+            // native name
+            var nativeName = countrydata.name.nativeName;
+            var nativeNameList = "";
+            for (let x in nativeName) {
+                if(x != "eng"){
+                    // console.log(nativeName[x].official);
+                    nativeNameList += `<i>${nativeName[x].official}</i>`;
+                }
+            }
+            document.getElementById("nativeNames").innerHTML = nativeNameList;
+            document.getElementById("commonName").innerText = countrydata.name.common;
+            document.getElementById("capitalName").innerText = countrydata.capital[0];
+            document.getElementById("contientName").innerText = countrydata.continents[0];
+    
+            // maps
+            var mapsHref = document.querySelectorAll(".mapsHdng a");
+            mapsHref[0].setAttribute("href", countrydata.maps.googleMaps);
+            mapsHref[1].setAttribute("href", countrydata.maps.openStreetMaps);
+    
+            // other information
+            let popinCommas = addCommas(countrydata.population);
+            document.getElementById("population").innerText = popinCommas;
+            let areainCommas = addCommas(countrydata.area);
+            document.getElementById("area").innerHTML = areainCommas + " km<sup>2</sup>";
+            document.getElementById("idd").innerText = countrydata.idd.root + countrydata.idd.suffixes[0];
+            document.getElementById("tld").innerText = countrydata.tld;
+    
+            // is un member 
+            if(countrydata.unMember == true){
+                document.getElementById("unMember").innerHTML = "<i class='info'>yes</i>";
+            } else{
+                document.getElementById("unMember").innerHTML = "<i class='warning'>no</i>";
+            }
+    
+            // currency 
+            let currHTML = "";
+            let currList = countrydata.currencies;
+            for (let x in currList) {
+                currHTML += `<i class="currname">${currList[x].name}</i>`
+            }
+    
+            document.getElementById("curr").innerHTML = currHTML;
+            document.getElementById("carSide").innerText = countrydata.car.side;
+            document.getElementById("startOfWeek").innerText = countrydata.startOfWeek;
+    
+        }
+        // add comma to a bigger numbers
+        function addCommas(number) {
+            let numberString = number.toString();
+            numberString = numberString.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            return numberString;
+        }
+    } else{
+        window.location.href = "./countries.htm";
+    }
 }
